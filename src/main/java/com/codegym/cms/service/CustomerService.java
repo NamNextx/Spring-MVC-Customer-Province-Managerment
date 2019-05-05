ab0ab6449c1d66a6fbfdb16f0a79919acf73c60d
Namnextx <phuongnam.nextx@gmail.com>
2019-05-05 16:08:15 +0700

    edit file CustomerController: add Page findAll

--- src/main/java/com/codegym/cms/service/CustomerService.java
+++ src/main/java/com/codegym/cms/service/CustomerService.java
@@ -2,13 +2,21 @@ package com.codegym.cms.service;
 
 import com.codegym.cms.model.Customer;
 import com.codegym.cms.model.Province;
-
-import java.util.Iterator;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 public interface CustomerService {
+    Page<Customer> findAll(Pageable pageable);
+
     Iterable<Customer> findAll();
-    Customer findById(Long id) ;
-     void save(Customer customer);
-     void remove(Long id);
-     Iterable<Customer> findByProvince(Province province);
+
+    Customer findById(Long id);
+
+    void save(Customer customer);
+
+    void remove(Long id);
+
+    Iterable<Customer> findByProvince(Province province);
+
+    Page<Customer> findAllByFirstNameContaining(String firstname, Pageable pageable);
 }
